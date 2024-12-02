8b262530cde684de766441bb557622eb23d93a2d
Daniel Pecos Martinez <me@danielpecos.com>
2024-12-02 15:29:57 +0100

    Day 2 - refactored to use structured concurrency

--- src/tech/dplabs/aoc/day2/Day2Star2.java
+++ src/tech/dplabs/aoc/day2/Day2Star2.java
@@ -1,21 +1,79 @@
 package tech.dplabs.aoc.day2;
 
+import java.util.ArrayList;
+import java.util.concurrent.*;
+import java.util.function.Supplier;
+
 import static tech.dplabs.aoc.day2.ReportUtils.isReportSafe;
 import static tech.dplabs.aoc.day2.ReportUtils.readReportsFromFile;
 
 public class Day2Star2 {
 
   public static void main(String[] args) {
+    var startTime = System.currentTimeMillis();
+
     var reports = readReportsFromFile("src/tech/dplabs/aoc/day2/input.txt");
 
     var safeReports = 0;
 
-    for (var report : reports) {
-      if (isReportSafe(report, 1)) {
-        safeReports++;
+    /* Single threaded */
+//    for (var report : reports) {
+//      if (isReportSafe(report, 1)) {
+//        safeReports++;
+//      }
+//    }
+
+    /* Virtual threads - Executors API */
+//    try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+//      var jobs = new ArrayList<Future<Boolean>>();
+//
+//      for (var report : reports) {
+//        jobs.add(executor.submit(() -> {
+//            return isReportSafe(report, 1);
+//          })
+//        );
+//      }
+//
+//      for (var job : jobs) {
+//        try {
+//          if (job.get()) {
+//            safeReports++;
+//          }
+//        } catch (Exception e) {
+//          throw new RuntimeException(e);
+//        }
+//      }
+//    }
+
+    /* Virtual threads - Structured Tasks API */
+    try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
+      var jobs = new ArrayList<Supplier<Boolean>>();
+
+      for (var report : reports) {
+        jobs.add(scope.fork(() -> isReportSafe(report, 1)));
       }
+
+      scope.join();
+      scope.throwIfFailed();
+
+      for (var job : jobs) {
+        try {
+          if (job.get()) {
+            safeReports++;
+          }
+        } catch (Exception e) {
+          throw new RuntimeException(e);
+        }
+      }
+
+    } catch (ExecutionException e) {
+      throw new RuntimeException(e);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
     }
 
     System.out.println(safeReports);
+
+    System.out.println("Total execution time: " + (System.currentTimeMillis() - startTime) + "ms");
   }
 }
