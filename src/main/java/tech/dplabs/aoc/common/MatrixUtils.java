490f65ddf22a28aac7e7e747acc9bc35e6c4e013
Daniel Pecos Martinez <me@danielpecos.com>
2024-12-08 22:44:22 +0100

    Matrix utils refactored

--- src/main/java/tech/dplabs/aoc/common/MatrixUtils.java
+++ src/main/java/tech/dplabs/aoc/common/MatrixUtils.java
@@ -1,30 +1,29 @@
 package tech.dplabs.aoc.common;
 
 public class MatrixUtils {
+  public static <T> boolean withinBoundaries(T[][] matrix, int row, int col) {
+    return (row < 0 || row >= matrix.length || col < 0 || col >= matrix[row].length);
+  }
 
   public static <T> boolean checkPosition(T[][] matrix, int row, int col, T value) {
-    if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[row].length) {
+    if (withinBoundaries(matrix, row, col)) {
       return false;
     }
     return matrix[row][col].equals(value);
   }
 
   public static <T> T[][] setPosition(T[][] matrix, int row, int col, T value) {
-    if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[row].length) {
+    if (withinBoundaries(matrix, row, col)) {
       return matrix;
     }
     matrix[row][col] = value;
     return matrix;
   }
 
-  public static <T> void printMatrix(T[][] matrix) {
+  public static <T> void print(T[][] matrix) {
     for (var row : matrix) {
       for (var col : row) {
-        if (col == null) {
-          System.out.print(" . ");
-        } else {
-          System.out.print(" " + col + " ");
-        }
+        System.out.print(" " + (col != null ? col : ".") + " ");
       }
       System.out.println();
     }
